d2e8aa7e650b58587fee103a5dc312c135d05509
youssefbello <youssef.benlallahom@esprit.tn>
2024-08-22 18:45:19 +0100

    nothing new

--- front/src/Layout.js
+++ front/src/Layout.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import Header from './components/dashboard/Header'; // Import your Header component
 import './App.css'
 const Layout = ({ children }) => {
   return (
     <>
-      <Header />
       <div className="main-content">
         {children}
       </div>
