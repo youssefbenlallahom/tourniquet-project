d8bfabd6d1bf92677eb6e20abae5cc4e81e4d5a3
youssefbello <youssef.benlallahom@esprit.tn>
2024-08-26 11:37:20 +0100

    update

--- front/src/components/dashboard/Assignment.js
+++ front/src/components/dashboard/Assignment.js
@@ -1,9 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import {
-  Box, Button, TextField, Typography, Modal, FormControl, InputLabel, 
-  IconButton, Table, TableBody, TableCell, TableContainer, TableHead, 
-  TableRow, Paper, Autocomplete, Select, MenuItem, Snackbar, CircularProgress, Fade
-} from '@mui/material';
+import { Box, Button, TextField, Typography, Modal, FormControl, InputLabel, List, ListItem, ListItemText, ListItemSecondaryAction, IconButton, Divider, Card, CardContent, Autocomplete, Select, MenuItem } from '@mui/material';
 import { Delete } from '@mui/icons-material';
 import axiosInstance from '../../axiosInstance';
 import Layout from '../../Layout';
@@ -16,44 +12,60 @@ const Assignment = () => {
   const [timezones, setTimezones] = useState([]);
   const [braceletId, setBraceletId] = useState('');
   const [color, setColor] = useState('');
-  const [name, setName] = useState('');
+  const [name, setName] = useState(''); // New state for name
   const [selectedRole, setSelectedRole] = useState('');
   const [selectedAccess, setSelectedAccess] = useState('');
   const [selectedTimezone, setSelectedTimezone] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [snackbar, setSnackbar] = useState({ open: false, message: '', severity: '' });
 
   useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
+    const fetchAssignments = async () => {
       try {
-        const [assignmentResponse, roleResponse, accessResponse, timezoneResponse] = await Promise.all([
-          axiosInstance.get('/assignment/all/'),
-          axiosInstance.get('/role/all/'),
-          axiosInstance.get('/access/all/'),
-          axiosInstance.get('/timezone/all/')
-        ]);
-        setAssignments(assignmentResponse.data);
-        setRoles(roleResponse.data);
-        setAccesses(accessResponse.data);
-        setTimezones(timezoneResponse.data);
+        const response = await axiosInstance.get('/assignment/all/');
+        setAssignments(response.data);
       } catch (error) {
-        console.error('Error fetching data:', error);
-      } finally {
-        setLoading(false);
+        console.error('Error fetching assignments:', error);
       }
     };
 
-    fetchData();
+    const fetchRoles = async () => {
+      try {
+        const response = await axiosInstance.get('/role/all/');
+        setRoles(response.data);
+      } catch (error) {
+        console.error('Error fetching roles:', error);
+      }
+    };
+
+    const fetchAccesses = async () => {
+      try {
+        const response = await axiosInstance.get('/access/all/');
+        setAccesses(response.data);
+      } catch (error) {
+        console.error('Error fetching accesses:', error);
+      }
+    };
+
+    const fetchTimezones = async () => {
+      try {
+        const response = await axiosInstance.get('/timezone/all/');
+        setTimezones(response.data);
+      } catch (error) {
+        console.error('Error fetching timezones:', error);
+      }
+    };
+
+    fetchAssignments();
+    fetchRoles();
+    fetchAccesses();
+    fetchTimezones();
   }, []);
 
   const handleAddAssignment = async () => {
-    setLoading(true);
     const newAssignment = {
       role: selectedRole,
       braceletId: braceletId,
       color: color,
-      name: name,
+      name: name, // Include the name field here
     };
 
     try {
@@ -64,181 +76,152 @@ const Assignment = () => {
           assignment_id: assignmentResponse.data.id,
           timezone_id: selectedTimezone?.id,
         };
+        
         await axiosInstance.post('/assignment_access/create/', newAssignmentAccess);
-        setAssignments([...assignments, assignmentResponse.data]);
-        setSnackbar({ open: true, message: 'Assignment created successfully', severity: 'success' });
+        
+        console.log('Assignment created successfully:', assignmentResponse.data);
         setOpen(false);
         setBraceletId('');
         setColor('');
-        setName('');
+        setName(''); // Reset name after submission
         setSelectedRole('');
         setSelectedAccess('');
         setSelectedTimezone(null);
+        setAssignments([...assignments, assignmentResponse.data]);
       } else {
-        setSnackbar({ open: true, message: 'Failed to create assignment', severity: 'error' });
+        console.error('Failed to create assignment:', assignmentResponse.statusText);
       }
     } catch (error) {
-      setSnackbar({ open: true, message: `Error: ${error.message}`, severity: 'error' });
-    } finally {
-      setLoading(false);
+      console.error('Error:', error);
     }
   };
 
   const handleRemoveAssignment = async (assignmentId) => {
-    setLoading(true);
     try {
       const response = await axiosInstance.delete(`/assignment/delete/${assignmentId}/`);
       if (response.status === 204) {
+        console.log('Assignment removed successfully');
         setAssignments(assignments.filter(assignment => assignment.id !== assignmentId));
-        setSnackbar({ open: true, message: 'Assignment removed successfully', severity: 'success' });
       } else {
-        setSnackbar({ open: true, message: 'Failed to remove assignment', severity: 'error' });
+        console.error('Failed to remove assignment:', response.statusText);
       }
     } catch (error) {
-      setSnackbar({ open: true, message: `Error: ${error.message}`, severity: 'error' });
-    } finally {
-      setLoading(false);
+      console.error('Error:', error);
     }
   };
 
   return (
     <Layout>
       <Box p={4}>
-        <Typography variant="h5" gutterBottom>
-          Assignments
-        </Typography>
-        <Button variant="contained" color="primary" onClick={() => setOpen(true)}>
-          Add Assignment
-        </Button>
-
-        {loading ? (
-          <Box display="flex" justifyContent="center" mt={4}>
-            <CircularProgress />
+        <Box display="flex" justifyContent="space-between" mb={2}>
+          <Button variant="contained" color="primary" onClick={() => setOpen(true)}>
+            Add Assignment
+          </Button>
+        </Box>
+        <Modal open={open} onClose={() => setOpen(false)}>
+          <Box
+            sx={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              width: 400,
+              bgcolor: 'background.paper',
+              p: 4,
+              borderRadius: 2,
+              boxShadow: 3,
+            }}
+          >
+            <Typography variant="h6" gutterBottom>
+              Add Assignment
+            </Typography>
+            <TextField
+              label="Name"
+              fullWidth
+              value={name}
+              onChange={(e) => setName(e.target.value)} // Update name state
+              sx={{ mb: 2 }}
+            />
+            <TextField
+              label="Bracelet ID"
+              fullWidth
+              value={braceletId}
+              onChange={(e) => setBraceletId(e.target.value)}
+              sx={{ mb: 2 }}
+            />
+            <TextField
+              label="Color"
+              fullWidth
+              value={color}
+              onChange={(e) => setColor(e.target.value)}
+              sx={{ mb: 2 }}
+            />
+            <FormControl fullWidth sx={{ mb: 2 }}>
+              <InputLabel>Role</InputLabel>
+              <Select
+                value={selectedRole}
+                onChange={(e) => setSelectedRole(e.target.value)}
+              >
+                {roles.map(role => (
+                  <MenuItem key={role.id} value={role.id}>
+                    {role.roleName}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <FormControl fullWidth sx={{ mb: 2 }}>
+              <InputLabel>Access</InputLabel>
+              <Select
+                value={selectedAccess}
+                onChange={(e) => setSelectedAccess(e.target.value)}
+              >
+                {accesses.map(access => (
+                  <MenuItem key={access.id} value={access.id}>
+                    {access.GameName}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <Autocomplete
+              value={selectedTimezone}
+              onChange={(event, newValue) => {
+                setSelectedTimezone(newValue);
+              }}
+              options={timezones}
+              getOptionLabel={(option) => `Timezone ID: ${option.TimezoneId} - ${new Date(option.startTime).toLocaleString()} to ${new Date(option.endTime).toLocaleString()}`}
+              renderInput={(params) => <TextField {...params} label="Timezone" variant="outlined" fullWidth />}
+            />
+            <Button variant="contained" color="primary" onClick={handleAddAssignment} sx={{ mt: 2 }}>
+              Add Assignment
+            </Button>
           </Box>
-        ) : (
-          <TableContainer component={Paper} sx={{ mt: 4 }}>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Name</TableCell>
-                  <TableCell>Bracelet ID</TableCell>
-                  <TableCell>Color</TableCell>
-                  <TableCell>Role</TableCell>
-                  <TableCell>Actions</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {assignments.map((assignment) => (
-                  <TableRow key={assignment.id}>
-                    <TableCell>{assignment.name}</TableCell>
-                    <TableCell>{assignment.braceletId}</TableCell>
-                    <TableCell>{assignment.color}</TableCell>
-                    <TableCell>{assignment.role}</TableCell>
-                    <TableCell>
+        </Modal>
+
+        <Box mt={4}>
+          <Typography variant="h6" gutterBottom>
+            Existing Assignments
+          </Typography>
+          <List>
+            {assignments.map((assignment) => (
+              <Card key={assignment.id} variant="outlined" sx={{ mb: 2 }}>
+                <CardContent>
+                  <ListItem>
+                    <ListItemText
+                      primary={`Bracelet ID: ${assignment.braceletId}, Color: ${assignment.color}, Name: ${assignment.name}`}
+                      secondary={`Role: ${assignment.role}`}
+                    />
+                    <ListItemSecondaryAction>
                       <IconButton edge="end" aria-label="delete" onClick={() => handleRemoveAssignment(assignment.id)}>
                         <Delete />
                       </IconButton>
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        )}
-
-        <Modal open={open} onClose={() => setOpen(false)}>
-          <Fade in={open} timeout={500}>
-            <Box
-              sx={{
-                position: 'absolute',
-                top: '50%',
-                left: '50%',
-                transform: 'translate(-50%, -50%)',
-                width: 550,
-                bgcolor: 'background.paper',
-                p: 4,
-                borderRadius: 2,
-                boxShadow: 3,
-              }}
-            >
-              <Typography variant="h6" gutterBottom>
-                Add Assignment
-              </Typography>
-              <TextField
-                label="Name"
-                fullWidth
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                sx={{ mb: 2 }}
-              />
-              <TextField
-                label="Bracelet ID"
-                fullWidth
-                value={braceletId}
-                onChange={(e) => setBraceletId(e.target.value)}
-                sx={{ mb: 2 }}
-              />
-              <TextField
-                label="Color"
-                fullWidth
-                value={color}
-                onChange={(e) => setColor(e.target.value)}
-                sx={{ mb: 2 }}
-              />
-              <FormControl fullWidth sx={{ mb: 2 }}>
-                <InputLabel>Role</InputLabel>
-                <Select
-                  value={selectedRole}
-                  onChange={(e) => setSelectedRole(e.target.value)}
-                  
-                >
-                  {roles.map(role => (
-                    <MenuItem key={role.id} value={role.id}>
-                      {role.roleName}
-                      
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-              <FormControl fullWidth sx={{ mb: 2 }}>
-                <InputLabel>Access</InputLabel>
-                <Select
-                  value={selectedAccess}
-                  onChange={(e) => setSelectedAccess(e.target.value)}
-                >
-                  {accesses.map(access => (
-                    <MenuItem key={access.id} value={access.id}>
-                      {access.GameName}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-              <Autocomplete
-                value={selectedTimezone}
-                onChange={(event, newValue) => {
-                  setSelectedTimezone(newValue);
-                }}
-                options={timezones}
-                getOptionLabel={(option) => 
-                  `Start time:${new Date(option.startTime).toLocaleString()} - End time: ${new Date(option.endTime).toLocaleString()}`
-                }
-                renderInput={(params) => <TextField {...params} label="Timezone" variant="outlined" fullWidth />}
-              />
-              <Button variant="contained" color="primary" onClick={handleAddAssignment} sx={{ mt: 2 }}>
-                Add Assignment
-              </Button>
-            </Box>
-          </Fade>
-        </Modal>
-
-        <Snackbar
-          open={snackbar.open}
-          autoHideDuration={6000}
-          onClose={() => setSnackbar({ ...snackbar, open: false })}
-          message={snackbar.message}
-          anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
-          severity={snackbar.severity}
-        />
+                    </ListItemSecondaryAction>
+                  </ListItem>
+                </CardContent>
+                <Divider />
+              </Card>
+            ))}
+          </List>
+        </Box>
       </Box>
     </Layout>
   );
